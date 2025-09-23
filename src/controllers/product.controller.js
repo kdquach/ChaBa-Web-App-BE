1c4427f71b8888d780384d01e67dfaf2940a580a
kdquach <kd.quach03@gmail.com>
2025-09-23 15:21:55 +0700

    feat: done create product service and controller

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,39 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-unused-vars */
+const httpStatus = require('http-status');
+const pick = require('../utils/pick');
+const ApiError = require('../utils/ApiError');
+const catchAsync = require('../utils/catchAsync');
+const { productService } = require('../services');
+
+const createProduct = catchAsync(async (req, res) => {
+  const product = await productService.createProduct(req.body);
+  res.status(httpStatus.CREATED).send(product);
+});
+const getProducts = catchAsync(async (req, res) => {
+  const products = await productService.queryProducts(req.query);
+  res.status(httpStatus.OK).send(products);
+});
+const getProduct = catchAsync(async (req, res) => {
+  const product = await productService.getProductById(req.params.productId);
+  if (!product) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Product not found');
+  }
+  res.status(httpStatus.OK).send(product);
+});
+const updateProduct = catchAsync(async (req, res) => {
+  const product = await productService.updateProductById(req.params.productId, req.body);
+  res.status(httpStatus.OK).send(product);
+});
+const deleteProduct = catchAsync(async (req, res) => {
+  await productService.deleteProductById(req.params.productId);
+  res.status(httpStatus.NO_CONTENT).send();
+});
+
+module.exports = {
+  createProduct,
+  getProducts,
+  getProduct,
+  updateProduct,
+  deleteProduct,
+};
