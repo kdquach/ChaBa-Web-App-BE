4edc932d37f1db44304586165e67a2c828a3c196
LocMTCE181795 <LocMTCE181795@fpt.edu.vn>
2025-10-17 23:12:29 +0700

    feat/category: prevent duplicate names (case-insensitive)

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -10,7 +10,38 @@ const getCategories = catchAsync(async (req, res) => {
   const categories = await categoryService.queryCategories(filter, options);
   res.status(httpStatus.OK).send(categories);
 });
+const getCategory = catchAsync(async (req, res) => {
+  const category = await categoryService.getCategoryById(req.params.categoryId);
+  if (!category) {
+    throw new ApiError(httpStatus.NOT_FOUND, "Category not found");
+  }
+  res.status(httpStatus.OK).send(category);
+});
+
+const createCategory = catchAsync(async (req, res) => {
+  const categoryData = req.body;
+  const category = await categoryService.createCategory(categoryData);
+  res.status(httpStatus.CREATED).send(category);
+});
+
+const updateCategory = catchAsync(async (req, res) => {
+  const updateData = req.body;
+  const category = await categoryService.updateCategoryById(
+    req.params.categoryId,
+    updateData
+  );
+  res.status(httpStatus.OK).send(category);
+});
+
+const deleteCategory = catchAsync(async (req, res) => {
+  await categoryService.deleteCategoryById(req.params.categoryId);
+  res.status(httpStatus.NO_CONTENT).send();
+});
 
 module.exports = {
   getCategories,
+  createCategory,
+  getCategory,
+  updateCategory,
+  deleteCategory,
 };
