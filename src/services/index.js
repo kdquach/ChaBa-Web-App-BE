206cce3284ea0a51f192646cfa342106f478c60c
Le Thanh Tra <lethanhtra06092004@gmail.com>
2025-10-21 08:15:47 +0700

    Thêm module cart và bổ sung role cho loại nguyên liệu..

--- src/services/index.js
+++ src/services/index.js
@@ -5,5 +5,7 @@ module.exports.userService = require("./user.service");
 module.exports.productService = require("./product.service");
 module.exports.categoryService = require("./category.service");
 module.exports.ingredientService = require("./ingredient.service");
+module.exports.ingredientCategoryService = require("./ingredientCategory.service");
+module.exports.cartService = require("./cart.service");
 module.exports.toppingService = require("./topping.service");
 module.exports.otpService = require("./otp.service");
